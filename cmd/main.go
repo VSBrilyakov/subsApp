69cb2041c87b9152d52d8921dfec1d5a38509f88
Vova <YesItsVova@mail.ru>
2026-02-09 17:17:16 +0300

    A simple HTTP server run

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"githhub.com/VSBrilyakov/test-app/internal"
+)
+
+func main() {
+	srv := new(internal.Server)
+	if err := srv.Run("8080"); err != nil {
+		log.Fatalf("server run failed: %s", err.Error())
+	}
 }
